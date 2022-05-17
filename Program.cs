b55d1db041ed48eda898dad5c44708d0d5dca060
gmmurray <trinilime123@live.com>
2022-05-17 03:06:43 -0400

    more initial work

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Brainstorm.V2.Frontend;
 using Brainstorm.V2.Frontend.Extensions;
+using Brainstorm.V2.Frontend.Extensions.AuthExtensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -15,11 +16,12 @@ builder.Services.AddHttpClient("WebAPI", client => client.BaseAddress = new Uri(
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WebAPI"));
 
-builder.Services.AddOidcAuthentication(options =>
+builder.Services.AddAuth0OidcAuthentication(options =>
 {
   builder.Configuration.Bind("Auth0", options.ProviderOptions);
   options.ProviderOptions.ResponseType = "code";
   options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
+  options.ProviderOptions.MetadataSeed.EndSessionEndpoint = $"{builder.Configuration["Auth0:Authority"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}&returnTo={builder.HostEnvironment.BaseAddress}";
 });
 
 await builder.Build().RunAsync();
